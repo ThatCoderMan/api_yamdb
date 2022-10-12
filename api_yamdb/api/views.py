66f5107224911f274b20f5e2be1e40a5f3a0aca3
Артемий Березин <artemii@MacBook-Air-Artemij.local>
2022-10-12 12:49:41 +0300

    add edpoints

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.mixins import (CreateModelMixin,
+                                   ListModelMixin,
+                                   DestroyModelMixin)
 
-# Create your views here.
+from reviews.models import Category, Genre, Title, Review, Comment
+
+
+class CreateListDestroyModelViewSet(CreateModelMixin,
+                                    ListModelMixin,
+                                    DestroyModelMixin,
+                                    viewsets.GenericViewSet):
+    pass
+
+
+class CategoryViewSet(CreateListDestroyModelViewSet):
+    queryset = Category.objects.all()
+
+
+class GenreViewSet(CreateListDestroyModelViewSet):
+    queryset = Genre.objects.all()
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    http_method_names = ['get', 'post', 'patch', 'del']  # we don't use put
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    http_method_names = ['get', 'post', 'patch', 'del']  # we don't use put
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    http_method_names = ['get', 'post', 'patch', 'del']  # we don't use put
--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +25,5 @@ urlpatterns = [
         TemplateView.as_view(template_name='redoc.html'),
         name='redoc'
     ),
+    path('api/v1/', include('api.urls'))
 ]
