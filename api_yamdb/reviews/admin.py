7663a95286ea718d4a329b7d7fb830bd96d4dbaa
Вячеслав <i@omen-121.ru>
2022-10-16 00:21:20 +0300

    customization admin panel

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,9 +1,60 @@
 from django.contrib import admin
 
-from .models import Category, Comment, Genre, Review, Title
+from .models import Category, Comment, Genre, Review, Title, User
 
-admin.site.register(Title)
-admin.site.register(Category)
-admin.site.register(Genre)
-admin.site.register(Review)
-admin.site.register(Comment)
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year', 'category', 'view_genre_list')
+    ordering = ('name',)
+    list_filter = ('year', 'category', 'genre', )
+    search_fields = ('name__startswith',
+                     'year',
+                     'category__name__startswith',
+                     'genre__name__startswith')
+
+    def view_genre_list(self, obj):
+        genres = Title.genre.through.objects.filter(title_id=obj.id)
+        genre_list = ''
+        for genre in genres:
+            genre = Genre.objects.get(pk=genre.genre_id)
+            genre_list += genre.name + ' '
+        return genre_list
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    ordering = ('name',)
+    search_fields = ('name__startswith',)
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    ordering = ('name',)
+    search_fields = ('name__startswith',)
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('title_id', 'score', 'author', 'text', 'pub_date')
+    ordering = ('-pub_date',)
+    list_filter = ('title_id', 'author', )
+    search_fields = ('title_id__name__startswith',
+                     'score',
+                     'author__username__startswith',
+                     'text')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('review_id', 'author', 'text', 'pub_date')
+    ordering = ('-pub_date',)
+    list_filter = ('author', 'review_id__title_id')
+    search_fields = ('review_id__author__username__startswith',
+                     'author__username__startswith',
+                     'text')
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'role')
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -20,7 +20,7 @@ class User(AbstractUser):
     username = models.CharField(
         verbose_name='user name',
         max_length=150,
-        validators=(RegexValidator(r'[\w.@+-]+\z'),),
+        validators=(RegexValidator(r'[\w.@+-]'),),
         unique=True,
     )
     email = models.EmailField(
@@ -110,6 +110,10 @@ class Review(models.Model):
         db_index=True,
     )
 
+    def __str__(self):
+        return (f'Обзор пользователя {self.author.username} '
+                f'на произведение "{self.title_id}"')
+
 
 class Comment(models.Model):
     review_id = models.ForeignKey(
@@ -125,3 +129,8 @@ class Comment(models.Model):
         auto_now_add=True,
         db_index=True,
     )
+
+    def __str__(self):
+        return ('Комментарий к обзору пользователя '
+                f'{self.review_id.author.username} на '
+                f'"{self.review_id.title_id}"')
