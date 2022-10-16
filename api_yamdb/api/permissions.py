a247444d727c99a02e0aef56a94b85b2ab7a0f9b
Artemii Berezin <31103191+ThatCoderMan@users.noreply.github.com>
2022-10-16 23:18:43 +0300

    Update permissions.py

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -6,12 +6,10 @@ ADMIN_MODERATOR_ROLES = ('admin', 'moderator')
 class IsAdminOrReadOnly(metaclass=permissions.BasePermissionMetaclass):
 
     def has_permission(self, request, view):
-        return (
-                request.method in permissions.SAFE_METHODS
+        return (request.method in permissions.SAFE_METHODS
                 or request.user.is_authenticated
                 and (request.user.role == 'admin'
-                     or request.user.is_superuser)
-                )
+                     or request.user.is_superuser))
 
     def has_object_permission(self, request, view, obj):
         return True
@@ -20,10 +18,8 @@ class IsAdminOrReadOnly(metaclass=permissions.BasePermissionMetaclass):
 class IsAdmin(metaclass=permissions.BasePermissionMetaclass):
 
     def has_permission(self, request, view):
-        return (
-                request.user.role == 'admin'
-                or request.user.is_superuser
-                )
+        return (request.user.role == 'admin'
+                or request.user.is_superuser)
 
     def has_object_permission(self, request, view, obj):
         return True
