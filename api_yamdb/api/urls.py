9735659b482d75a1d4f9bc65df0148172189f27a
Артемий Березин <artemii@MacBook-Air-Artemij.local>
2022-10-16 03:16:42 +0300

    add permissions; some tests

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
 
-from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
+from .views import (CategoryViewSet, CommentViewSet, GenreViewSet, UserViewSet,
                     ReviewViewSet, SignUpView, TitleViewSet, TokenView)
 
 router = SimpleRouter()
@@ -15,14 +15,15 @@ review_router.register('reviews', ReviewViewSet, basename='reviews')
 comment_router = SimpleRouter()
 comment_router.register('comments', CommentViewSet, basename='comments')
 
+user_router = SimpleRouter()
+user_router.register('', UserViewSet, basename='users')
 
 urlpatterns = [
     path('', include(router.urls)),
     path('titles/<int:title_id>/', include(review_router.urls)),
     path('titles/<int:title_id>/reviews/<int:review_id>/',
          include(comment_router.urls)),
+    path('users/', include(user_router.urls)),
     path('auth/signup/', SignUpView.as_view()),
     path('auth/token/', TokenView.as_view())
-    # path('users/', ),  todo: add users urls
-    # path('users/<slug:username>', ),  todo: add user urls
 ]
