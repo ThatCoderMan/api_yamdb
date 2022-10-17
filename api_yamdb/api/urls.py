d11f9e537702afb2de806d5d7bbe52f878cc2c8e
Артемий Березин <artemii@MacBook-Air-Artemij.local>
2022-10-17 20:13:49 +0300

    refactor urls

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -18,10 +18,10 @@ comment_router = SimpleRouter()
 comment_router.register('comments', CommentViewSet, basename='comments')
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('titles/<int:title_id>/', include(review_router.urls)),
-    path('titles/<int:title_id>/reviews/<int:review_id>/',
+    path('v1/', include(router.urls)),
+    path('v1/titles/<int:title_id>/', include(review_router.urls)),
+    path('v1/titles/<int:title_id>/reviews/<int:review_id>/',
          include(comment_router.urls)),
-    path('auth/signup/', SignUpView.as_view()),
-    path('auth/token/', TokenView.as_view())
+    path('v1/auth/signup/', SignUpView.as_view()),
+    path('v1/auth/token/', TokenView.as_view())
 ]
